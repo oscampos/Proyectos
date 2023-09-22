f72c0e3546af42604a4355b1fc97f46bee7ebfaf
oscampos <108683335+oscampos@users.noreply.github.com>
2023-09-21 23:22:58 -0600

    Este sera la otra prueba funcional con el IP SLA , Track y Event Manager

--- SO Requerimiento enlace LGW-WxC caido.rb
+++ SO Requerimiento enlace LGW-WxC caido.rb
@@ -1,3 +1,8 @@
+/ Este sere¿a la prueba funcional 2 que se podria utilizar para mientras ya que la anterior se ejecuta demasiado /
+
+!
+configure terminal
+!
 voice translation-rule 10
 rule 10 /+507/ /+50372120580/
 exit
@@ -6,34 +11,64 @@ voice translation-profile Llamadas_WxC_Caida
 translate called 10
 exit
 !
+ip sla 1
+!
+icmp-echo 8.8.8.8 source-ip 10.2.2.250
+!
+request-data-size 32
+!
+frequency 60
+!
+timeout 2000
+!
+ip sla schedule 1 life forever start-time now  
+!
+end
+wr
+!
+!
+config t
+!
+track 1 ip sla 1 state
+!
+delay up 10 down 5
+!
+track 2 ip sla 1 state
+!
+delay up 10 down 5
+!
+end
+wr
+!
+!
+configt terminal
+!
+event manager applet Enlace_WxC_Caido
+ event track 1 state down
+ action 0001 syslog priority critical msg "La troncal esta caida"
+ action 0002 cli command "config t"
+ action 0003 cli command "dial-peer voice 301 voip"
+ action 0003 cli command "translation-profile outgoing Llamadas_WxC_Caida"
+ action 0005 cli command "exit"
+ action 0006 cli command "dial-peer voice 300 voip"
+ action 0007 cli command "no destination dpg 200"
+ action 0008 cli command "destination dpg 300"
+ action 0009 cli command "end"
+!
+ exit
+!
+event manager applet Enlace_WxC_arriba
+ event track 2 state up
+ action 0001 cli command "config t"
+ action 0002 cli command "dial-peer voice 301 voip"
+ action 0003 cli command "no translation-profile outgoing Llamadas_WxC_Caida"
+ action 0004 cli command "exit"
+ action 0005 cli command "dial-peer voice 300 voip"
+ action 0006 cli command "no destination dpg 300"
+ action 0007 cli command "destination dpg 200"
+ action 0008 cli command "end"
+!
+end
+wr
+!
 
-
- event manager applet Enlace_WxC_Caido
- event timer watchdog time 12000
- action 0001 cli command "enable"
- action 0002 cli command "show sip-ua register status | include yes"
- action 0003 regexp "[0-9]+.*yes" "$_cli_result"
- action 0004 if $_regexp_result eq "1"
- action 0005 syslog priority notifications msg "La troncal esta arriba"
- action 0006 cli command "config t"
- action 0007 cli command "dial-peer voice 301 voip"
- action 0008 cli command "no translation-profile outgoing Llamadas_WxC_Caida"
- action 0009 cli command "exit"
- action 0010 cli command "dial-peer voice 300 voip"
- action 0011 cli command "no destination dpg 300"
- action 0012 cli command "destination dpg 200"
- action 0013 cli command "end"
- action 0014 end
- action 0015 if $_regexp_result eq "0"
- action 0016 syslog priority critical msg "La troncal esta caida"
- action 0017 cli command "config t"
- action 0018 cli command "dial-peer voice 301 voip"
- action 0019 cli command "translation-profile outgoing Llamadas_WxC_Caida"
- action 0020 cli command "exit"
- action 0021 cli command "dial-peer voice 300 voip"
- action 0022 cli command "no destination dpg 200"
- action 0023 cli command "destination dpg 300"
- action 0024 cli command "end"
- action 0025 end
-
- 
